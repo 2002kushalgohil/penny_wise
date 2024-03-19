17652a3f918014a45b82448ac194d89247fd679c
Kushal Gohil <2002kushalgohil@gmail.com>
2024-03-19 18:20:03 +0530

    Configured Tailwind CSS and update documemtation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div className="h-full flex items-center justify-center">
-      <h1 className="text-4xl md:text-8xl">Hello World</h1>
-    </div>
+    <div></div>
   );
 }
 
