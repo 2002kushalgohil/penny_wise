689ce193f46e302636f50bee08cdd771f119350a
Kushal Gohil <2002kushalgohil@gmail.com>
2024-03-20 10:01:56 +0530

    UI Improvement

--- src/app/(landingPage)/_components/Hero.tsx
+++ src/app/(landingPage)/_components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(#ffffff33_1px,#0D0D0D_1px)] bg-[size:30px_30px] flex items-center justify-center flex-col gap-4 md:gap-10">
+    <div className="h-screen w-screen bg-[radial-gradient(#ffffff33_1px,#0D0D0D_1px)] bg-[size:30px_30px] flex items-center justify-center flex-col gap-4 md:gap-10">
       <img src="logo.png" className="w-10 md:w-20"/>
         <h1 className="text-4xl md:text-8xl">Penny Wise</h1>
         <p className="text-lg md:text-4xl">Comming Soon...</p>
